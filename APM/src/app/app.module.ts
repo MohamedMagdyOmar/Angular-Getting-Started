e06a2c0733be6c5cc8e8925b1e0cb7d814bdb29f
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-06-03 15:42:13 -0400

    Building Custom Pipes

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
   bootstrap: [AppComponent]
 })
 
