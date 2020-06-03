4334a658c72c62510799b2eaa1031b4460bd1904
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-06-03 16:00:23 -0400

    Filtering List

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -13,12 +13,26 @@ export class ProductListComponent implements OnInit{
   ngOnInit(): void {
     console.log("OnInit");
   }
-  
+
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    //listFilter: string = 'cart';
+
+    _listFilter: string;
+    get listFilter(): string{
+      return this._listFilter
+    }
+
+    set listFilter(value:string){
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter? this.performFilter(this.listFilter): this.products;
+    }
+
+    // this will have filtered list of products
+    filteredProducts: IProduct[];
+
     products: IProduct[] = [
         {
           "productId": 1,
@@ -72,8 +86,18 @@ export class ProductListComponent implements OnInit{
         }
       ]
 
+      constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart'
+      }
+
       toggleImage(): void
       {
         this.showImage = !this.showImage;
+      }
+
+      performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
       }
 }
