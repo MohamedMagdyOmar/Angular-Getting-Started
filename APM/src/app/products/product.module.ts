295c0b8a4b177477e52610b81e32f871889d1b1d
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-06-06 13:00:55 -0400

    Shared Module

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-space.pipe';
-import { starComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    starComponent
+    ConvertToSpacesPipe
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     // we do not need to register a service more than once, so when we use forRoot to pass in our 
     // configured routes, the RouterModule knows to register the router service provider
     // when we use forChild the RouterModule knows not to re-register the router service
@@ -27,7 +22,8 @@ import { ProductDetailGuard } from './product-detail.guard';
       {path: 'products/:id',
       canActivate: [ProductDetailGuard],
       component: ProductDetailComponent},
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
